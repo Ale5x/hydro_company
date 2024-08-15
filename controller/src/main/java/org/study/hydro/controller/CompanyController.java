e41c371b1365effb632ebc3f633ebe74f2ab337f
Alexandr <58393300+Ale5x@users.noreply.github.com>
2024-08-15 22:28:33 +0300

    added controller docs for user and company

--- controller/src/main/java/org/study/hydro/controller/CompanyController.java
+++ controller/src/main/java/org/study/hydro/controller/CompanyController.java
@@ -13,15 +13,24 @@ import org.study.hydro.utill.ValidatorParam;
 
 import java.util.List;
 
+/**
+ * This class {@link CompanyController} provides access endpoints for user data.
+ *
+ * @author Aliaksandr Pishchala
+ */
 @RestController
 public class CompanyController {
 
     @Autowired
     private CompanyService companyService;
 
+    /**
+     * The method creates an access point for creating a company in the database.
+     * @param company is the date of the new company.
+     * @return The instance of ResponseEntity with the HttpStatus.
+     */
     @PostMapping(value = PathPages.COMPANY_CREATE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HttpStatus> create (@RequestBody CompanyDto company) {
-
         if (companyService.create(company)) {
             return new ResponseEntity<>(HttpStatus.CREATED);
         } else {
@@ -29,6 +38,12 @@ public class CompanyController {
         }
     }
 
+    /**
+     * The method returns all companies data by page.
+     * @param page is the current page.
+     * @param size is the count items on this page.
+     * @return The object of the CollectionModel includes list of companies.
+     */
     @GetMapping(value = PathPages.COMPANY_ALL, produces = MediaType.APPLICATION_JSON_VALUE)
     public CollectionModel<CompanyDto> getCompanies(@RequestParam(ControllerConstants.PAGE) String page,
                                                     @RequestParam(ControllerConstants.SIZE) String size) {
@@ -38,6 +53,11 @@ public class CompanyController {
         return CollectionModel.of(companyDtoList);
     }
 
+    /**
+     * The method creates an and point for getting the company by company's id.
+     * @param id is the id of the company.
+     * @return The object of the companyDTO.
+     */
     @GetMapping(value = PathPages.COMPANY_ID, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public CompanyDto findCompanyById(@RequestParam(ControllerConstants.ID) String id) {
@@ -46,6 +66,11 @@ public class CompanyController {
                 new AppRequestException("Company doesn't exist", HttpStatus.BAD_REQUEST));
     }
 
+    /**
+     * The method creates an and point for getting the companies by company's name.
+     * @param name is the name of some company.
+     * @return The list of the companiesDTO.
+     */
     @GetMapping(value = PathPages.COMPANY_BY_NAME, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<CompanyDto> findCompanyByName(@RequestParam(ControllerConstants.NAME) String name) {
         return companyService.findByName(name);
--- controller/src/main/java/org/study/hydro/controller/UserController.java
+++ controller/src/main/java/org/study/hydro/controller/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.study.hydro.entity.Dto.UserDto;
+import org.study.hydro.exception.AppExceptionHandler;
 import org.study.hydro.exception.AppRequestException;
+import org.study.hydro.exception.CoreException;
 import org.study.hydro.service.UserService;
 import org.study.hydro.utill.Pagination;
 import org.study.hydro.utill.ValidatorParam;
@@ -18,28 +20,35 @@ import java.util.List;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+/**
+ * This class {@link UserController} provides access endpoints for user data.
+ *
+ * @author Aliaksandr Pishchala
+ */
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-//    @PostMapping(value = PathPages.AUTH_CREATE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<HttpStatus> createUser(@RequestBody UserDto user) {
-//        if (userService.create(user)) {
-//            return new ResponseEntity<>(HttpStatus.CREATED);
-//        }
-//        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//    }
-
+    /**
+     * The method gives user data by user's id.
+     * @param id is the id of the user.
+     * @return The object of the userDTO.
+     */
     @GetMapping(value = PathPages.USER_ID, produces = MediaType.APPLICATION_JSON_VALUE)
     public UserDto getUser(@RequestParam(ControllerConstants.ID) String id) {
         ValidatorParam.isNumber(id);
-        System.out.println(userService.findUserById(Integer.parseInt(id)));
         return userService.findUserById(Integer.parseInt(id))
                 .orElseThrow(() -> new AppRequestException("User is not found", HttpStatus.BAD_REQUEST));
     }
 
+    /**
+     * The method returns all users data by page.
+     * @param page is the current page.
+     * @param size is the count items on this page.
+     * @return The object of the CollectionModel includes list of users.
+     */
     @GetMapping(value = PathPages.USER_ALL, produces = MediaType.APPLICATION_JSON_VALUE)
     public CollectionModel<UserDto> getUsers(@RequestParam(ControllerConstants.PAGE) String page,
                                              @RequestParam(ControllerConstants.SIZE) String size) {
@@ -52,12 +61,24 @@ public class UserController {
                 getNextLinkForGetUsers(page, size));
     }
 
+    /**
+     * The method creates the previous link.
+     * @param page is the current page.
+     * @param size is the count items on this page.
+     * @return The instance of link.
+     */
     private Link getPreviousLinkForGetUsers(String page, String size) {
         return linkTo(methodOn(UserController.class)
                 .getUsers(Pagination.getPreviousPage(page), size))
                 .withRel(ControllerConstants.PREVIOUS).withType(ControllerConstants.METHOD_GET);
     }
 
+    /**
+     * The method creates the next link.
+     * @param page is the current page.
+     * @param size is the count items on this page.
+     * @return The instance of link.
+     */
     private Link getNextLinkForGetUsers(String page, String size) {
         if(Pagination.isNextListEmpty(
                 Collections.singletonList(userService.findAll(Integer.parseInt(size),

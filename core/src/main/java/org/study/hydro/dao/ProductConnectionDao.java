e47d4876612fb2251d8dcfcb08327d0ab1e677ba
Alexandr <58393300+Ale5x@users.noreply.github.com>
2024-11-10 22:09:48 +0300

    added the product-connection dao and its impl files

---
+++ core/src/main/java/org/study/hydro/dao/ProductConnectionDao.java
@@ -0,0 +1,48 @@
+package org.study.hydro.dao;
+
+import org.study.hydro.entity.ProductConnection;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Interface {@link ProductConnectionDao} provides operation with data of database table 'products_connection'.
+ *
+ * @author Aliaksandr Pishchala
+ */
+public interface ProductConnectionDao {
+
+    /**
+     * The method creates the new record in the database table.
+     *
+     * @param productConnection entity that specifies the creation of new records in the database table.
+     *
+     * @return the ID of the newly created record in the table.
+     */
+    int save(ProductConnection productConnection);
+
+    /**
+     * The method returns the specified ProductConnection by id.
+     *
+     * @param id is the ProductConnection's id.
+     *
+     * @return the specified Optional ProductConnection by id.
+     */
+    Optional<ProductConnection> getProductConnectionById(int id);
+
+    /**
+     * The method will return list of the productConnections.
+     *
+     * @return the specified list of the productConnections.
+     */
+    List<ProductConnection> getListProductsConnection();
+
+    /**
+     * The method updates an existing table record in the database.
+     *
+     * @param productConnection entity having new data to update an existing table record.
+     *
+     * @return returns always a true result.
+     */
+    boolean updateProductConnection(ProductConnection productConnection);
+}
---
+++ core/src/main/java/org/study/hydro/dao/impl/ProductConnectionDaoImpl.java
@@ -0,0 +1,65 @@
+package org.study.hydro.dao.impl;
+
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.study.hydro.dao.CriteriaQueryHelper;
+import org.study.hydro.dao.ProductConnectionDao;
+import org.study.hydro.entity.ProductConnection;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public class ProductConnectionDaoImpl extends CriteriaQueryHelper<ProductConnection> implements ProductConnectionDao {
+
+    private final static String PRODUCT_CONNECTION_ID = "productConnectionId";
+
+    @Override
+    public int save(ProductConnection productConnection) {
+        Session session = getCurrentSession();
+
+        session.save(productConnection);
+        session.flush();
+        return productConnection.getProductConnectionId();
+    }
+
+    @Override
+    public Optional<ProductConnection> getProductConnectionById(int id) {
+        Session session = getCurrentSession();
+
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder(session);
+        CriteriaQuery<ProductConnection> criteriaQuery = getCriteriaQuery(criteriaBuilder, ProductConnection.class);
+        Root<ProductConnection> productConnectionRoot = getRoot(criteriaQuery, ProductConnection.class);
+
+        criteriaQuery.select(productConnectionRoot)
+                .where(criteriaBuilder.equal(productConnectionRoot.get(PRODUCT_CONNECTION_ID), id));
+        return session.createQuery(criteriaQuery).getResultList().stream().findFirst();
+    }
+
+    @Override
+    public List<ProductConnection> getListProductsConnection() {
+        Session session = getCurrentSession();
+
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder(session);
+        CriteriaQuery<ProductConnection> criteriaQuery = getCriteriaQuery(criteriaBuilder, ProductConnection.class);
+        Root<ProductConnection> criteriaQueryRoot = getRoot(criteriaQuery, ProductConnection.class);
+
+        criteriaQuery.select(criteriaQueryRoot);
+
+        return session.createQuery(criteriaQuery).getResultList();
+    }
+
+    @Override
+    public boolean updateProductConnection(ProductConnection productConnection) {
+        Session session = getCurrentSession();
+
+        session.update(productConnection);
+
+        return true;
+    }
+}

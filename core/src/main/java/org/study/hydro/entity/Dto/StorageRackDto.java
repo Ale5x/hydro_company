b605f70637fe5a4547eb8b65b78e65d32a869b36
Alexandr <58393300+Ale5x@users.noreply.github.com>
2024-11-10 22:12:29 +0300

    added the entities storage rack and its dto files

---
+++ core/src/main/java/org/study/hydro/entity/Dto/StorageRackDto.java
@@ -0,0 +1,67 @@
+package org.study.hydro.entity.Dto;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.Objects;
+
+public class StorageRackDto extends RepresentationModel<StorageRackDto> {
+
+    private int storageRackDtoId;
+    private String name;
+    private String shelfName;
+
+    public int getStorageRackDtoId() {
+        return storageRackDtoId;
+    }
+
+    public void setStorageRackDtoId(int storageRackDtoId) {
+        this.storageRackDtoId = storageRackDtoId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShelfName() {
+        return shelfName;
+    }
+
+    public void setShelfName(String shelfName) {
+        this.shelfName = shelfName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        StorageRackDto that = (StorageRackDto) o;
+
+        if (storageRackDtoId != that.storageRackDtoId) return false;
+        if (!Objects.equals(name, that.name)) return false;
+        return Objects.equals(shelfName, that.shelfName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + storageRackDtoId;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (shelfName != null ? shelfName.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "StorageRackDto{" +
+                "storageRackDtoId=" + storageRackDtoId +
+                ", name='" + name + '\'' +
+                ", shelfName='" + shelfName + '\'' +
+                '}';
+    }
+}
---
+++ core/src/main/java/org/study/hydro/entity/StorageRack.java
@@ -0,0 +1,115 @@
+package org.study.hydro.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "storage_racks")
+public class StorageRack implements Serializable {
+
+    private static final long serialVersionUID = -1850989911551433498L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_racks")
+    private int storageRackId;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "shelf_id")
+    private Shelf shelf;
+
+    @ManyToMany(mappedBy = "storageRackList")
+    private List<Product> productList;
+
+    public StorageRack() {
+    }
+
+    public StorageRack(int storageRackId, String name, Shelf shelf) {
+        this.storageRackId = storageRackId;
+        this.name = name;
+        this.shelf = shelf;
+    }
+
+    public StorageRack(String name, Shelf shelf) {
+        this.name = name;
+        this.shelf = shelf;
+    }
+
+    public StorageRack(String name) {
+        this.name = name;
+    }
+
+    public StorageRack(int storageRackId, String name) {
+        this.storageRackId = storageRackId;
+        this.name = name;
+    }
+
+    public int getStorageRackId() {
+        return storageRackId;
+    }
+
+    public void setStorageRackId(int storageRackId) {
+        this.storageRackId = storageRackId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Shelf getShelf() {
+        return shelf;
+    }
+
+    public void setShelf(Shelf shelf) {
+        this.shelf = shelf;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StorageRack that = (StorageRack) o;
+
+        if (storageRackId != that.storageRackId) return false;
+        if (!Objects.equals(name, that.name)) return false;
+        if (!Objects.equals(shelf, that.shelf)) return false;
+        return Objects.equals(productList, that.productList);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = storageRackId;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (shelf != null ? shelf.hashCode() : 0);
+        result = 31 * result + (productList != null ? productList.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "StorageRack{" +
+                "storageRackId=" + storageRackId +
+                ", name='" + name + '\'' +
+                ", shelf=" + shelf +
+                ", productList=" + productList +
+                '}';
+    }
+}

2f6103ccdd73a421d1d3632460aa7bc166287daa
Alexandr <58393300+Ale5x@users.noreply.github.com>
2024-11-10 22:31:52 +0300

    added the country service impl file

---
+++ core/src/main/java/org/study/hydro/service/impl/CountryServiceImpl.java
@@ -0,0 +1,72 @@
+package org.study.hydro.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.study.hydro.dao.CountryDao;
+import org.study.hydro.entity.Country;
+import org.study.hydro.entity.Dto.CountryDto;
+import org.study.hydro.exception.CoreException;
+import org.study.hydro.service.CountryService;
+import org.study.hydro.service.EntityMapper;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class CountryServiceImpl extends EntityMapper<CountryDto, Country> implements CountryService {
+
+    private static final String COUNTRY_NOT_FOUND_ERROR = "Country not found.";
+    private final CountryDao countryDao;
+
+    @Autowired
+    public CountryServiceImpl(CountryDao countryDao) {
+        this.countryDao = countryDao;
+    }
+
+    @Override
+    public List<CountryDto> findCountries() {
+        return mapToListObjectsDto(new ArrayList<>(countryDao.countries()))
+                .stream()
+                .sorted(Comparator.comparing(CountryDto::getName))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<CountryDto> findByName(String name) {
+        return mapToListObjectsDto(new ArrayList<>(countryDao.countriesByName(name)))
+                .stream()
+                .sorted(Comparator.comparing(CountryDto::getName))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<CountryDto> findById(int id) {
+        return Optional.of(mapToObjectDto(countryDao.countryById(id)
+                .orElseThrow(() -> new CoreException(COUNTRY_NOT_FOUND_ERROR))));
+    }
+
+    @Override
+    public List<CountryDto> mapToListObjectsDto(List<Country> objectsDtoList) {
+        List<CountryDto> countryDtoList = new ArrayList<>();
+        for(Country country : objectsDtoList) {
+            CountryDto countryDto = new CountryDto();
+            countryDto.setCountryId(country.getCountryId());
+            countryDto.setName(country.getName());
+            countryDtoList.add(countryDto);
+        }
+        return countryDtoList;
+    }
+
+    @Override
+    public CountryDto mapToObjectDto(Country object) {
+        CountryDto countryDto = new CountryDto();
+        countryDto.setCountryId(object.getCountryId());
+        countryDto.setName(object.getName());
+        return countryDto;
+    }
+
+    @Override
+    public Country mapToEntityFromDto(CountryDto objectDto, boolean isUpdate) {
+        return null;
+    }
+}

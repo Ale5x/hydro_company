adfb15662b9c0a00e758c849cad7c8e2ef9fc643
Alexandr <58393300+Ale5x@users.noreply.github.com>
2024-11-10 22:38:16 +0300

    added the shelf service file

---
+++ core/src/main/java/org/study/hydro/service/ShelfService.java
@@ -0,0 +1,56 @@
+package org.study.hydro.service;
+
+import org.study.hydro.entity.Dto.ShelfDto;
+import org.study.hydro.entity.Shelf;
+import org.study.hydro.exception.CoreException;
+
+import java.util.Optional;
+
+/**
+ * The ShelfService interface {@link ShelfService} provides methods for managing shelves.
+ * It defines basic operations for removing, updating etc. shelves.
+ *
+ * @author Aliaksandr Pishchala
+ */
+public interface ShelfService {
+
+    /**
+     * Updates the data of an existing shelf.
+     * @param shelfDto a {@link ShelfDto} object containing the updated shelf data.
+     * @return true if the shelf was successfully created; false otherwise.
+     * @throws CoreException if an error occurs during the retrieval process.
+     */
+    boolean update(ShelfDto shelfDto) throws CoreException;
+
+    /**
+     * Retrieves a shelfDto by its unique identifier.
+     * @param id the unique identifier of the shelf.
+     * @return an Optional object of {@link ShelfDto} object representing the shelf with the specified id.
+     * @throws CoreException if an error occurs during the retrieval process.
+     */
+    Optional<ShelfDto> findById(int id) throws CoreException;
+
+    /**
+     * Retrieves a shelf by its name.
+     * @param name the name of the shelf.
+     * @return an Optional object of {@link Shelf} object representing the shelf with its name.
+     * @throws CoreException if an error occurs during the retrieval process.
+     */
+    Optional<Shelf> findShelfByName(String name) throws CoreException;
+
+    /**
+     * Retrieves a shelf by its name.
+     * @param name the name of the shelf.
+     * @return an Optional object of {@link ShelfDto} object representing the shelf with its name.
+     * @throws CoreException if an error occurs during the retrieval process.
+     */
+    Optional<ShelfDto> findByName(String name) throws CoreException;
+
+    /**
+     * Deletes the data of an existing shelf.
+     * @param id the unique identifier of the shelf to be deleted.
+     * @return true if the shelf was successfully deleted; false otherwise.
+     * @throws CoreException if an error occurs during the retrieval process.
+     */
+    boolean remove(int id) throws CoreException;
+}

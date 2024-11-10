1ee5f9d90647fec9d4f507f06646cd630fe0b7f6
Alexandr <58393300+Ale5x@users.noreply.github.com>
2024-11-10 21:58:40 +0300

    added the entity country file

---
+++ core/src/main/java/org/study/hydro/entity/Country.java
@@ -0,0 +1,131 @@
+package org.study.hydro.entity;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "countries")
+public class Country implements Serializable {
+
+    private static final long serialVersionUID = -7401195441804755695L;
+
+    @Id
+    @Column(name = "id_countries")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int countryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "country", fetch = FetchType.LAZY)
+    private List<UserCompany> userCompanyList;
+
+    @ManyToMany(cascade = {CascadeType.ALL}, mappedBy = "companyCountries")
+    private List<ProductCompany> productCompanies;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "countryProduct")
+    private List<Product> productList = new ArrayList<>();
+
+    public Country() {
+    }
+
+    public Country(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public Country(int countryId, String name) {
+        this.countryId = countryId;
+        this.name = name;
+    }
+
+    public Country(int countryId, String name, List<UserCompany> userCompanyList) {
+        this.countryId = countryId;
+        this.name = name;
+        this.userCompanyList = userCompanyList;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<UserCompany> getUserCompanyList() {
+        return userCompanyList;
+    }
+
+    public void setUserCompanyList(List<UserCompany> userCompanyList) {
+        this.userCompanyList = userCompanyList;
+    }
+
+    public List<ProductCompany> getProductCompanies() {
+        return productCompanies;
+    }
+
+    public void setProductCompanies(List<ProductCompany> productCompanies) {
+        this.productCompanies = productCompanies;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Country country = (Country) o;
+
+        if (countryId != country.countryId) return false;
+        if (!Objects.equals(name, country.name)) return false;
+        if (!Objects.equals(userCompanyList, country.userCompanyList))
+            return false;
+        if (!Objects.equals(productCompanies, country.productCompanies))
+            return false;
+        return Objects.equals(productList, country.productList);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = countryId;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (userCompanyList != null ? userCompanyList.hashCode() : 0);
+        result = 31 * result + (productCompanies != null ? productCompanies.hashCode() : 0);
+        result = 31 * result + (productList != null ? productList.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "countryId=" + countryId +
+                ", name='" + name + '\'' +
+                ", userCompanyList=" + userCompanyList +
+                ", productCompanies=" + productCompanies +
+                ", productList=" + productList +
+                '}';
+    }
+}

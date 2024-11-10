d24684e5f6ec75cde9bd26bc6fb3be657959a6ed
Alexandr <58393300+Ale5x@users.noreply.github.com>
2024-11-10 22:11:45 +0300

    added the product-type dao and its impl files

---
+++ core/src/main/java/org/study/hydro/dao/ProductTypeDao.java
@@ -0,0 +1,66 @@
+package org.study.hydro.dao;
+
+import org.study.hydro.entity.ProductType;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Interface {@link ProductTypeDao} provides operation with data of database table 'products_type'.
+ *
+ * @author Aliaksandr Pishchala
+ */
+public interface ProductTypeDao {
+
+    /**
+     * The method creates new record in the database table.
+     *
+     * @param productType entity that specifies creation of the new record in database table.
+     *
+     * @return returns boolean's result if the row creates.
+     */
+    boolean create(ProductType productType);
+
+    /**
+     * The method will return list of the productTypes.
+     *
+     * @return the specified list of the productTypes.
+     */
+    List<ProductType> getProductTypes();
+
+    /**
+     * The method returns the specified productType by id.
+     *
+     * @param id is the productType's id.
+     *
+     * @return the specified Optional productType by id.
+     */
+    Optional<ProductType> getProductTypeById(int id);
+
+    /**
+     * The method returns the specified productType by name.
+     *
+     * @param name is the productType's name.
+     *
+     * @return the specified Optional productType by name.
+     */
+    Optional<ProductType> getProductTypeByName(String name);
+
+    /**
+     * The method updates an existing table record in the database.
+     *
+     * @param productType entity having new data to update an existing table record.
+     *
+     * @return returns always a true result.
+     */
+    boolean update (ProductType productType);
+
+    /**
+     * The method removes the current record in the database table.
+     *
+     * @param id is the id of the productType that will be removed.
+     *
+     * @return returns boolean's result if the row removes.
+     */
+    boolean remove (int id);
+}
---
+++ core/src/main/java/org/study/hydro/dao/impl/ProductTypeDaoImpl.java
@@ -0,0 +1,82 @@
+package org.study.hydro.dao.impl;
+
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.study.hydro.dao.CriteriaQueryHelper;
+import org.study.hydro.dao.ProductTypeDao;
+import org.study.hydro.entity.ProductType;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public class ProductTypeDaoImpl extends CriteriaQueryHelper<ProductType> implements ProductTypeDao {
+
+    private final static String PRODUCT_TYPE_ID = "productTypeId";
+    private final static String PRODUCT_TYPE_NAME = "name";
+
+    private final static String DELETE_PRODUCT_TYPE_QUERY = String.format("DELETE ProductType WHERE id =: %s", PRODUCT_TYPE_ID);
+
+    @Override
+    public boolean create(ProductType productType) {
+        Session session = getCurrentSession();
+        session.save(productType);
+        session.flush();
+        return productType.getProductTypeId() > 0;
+    }
+
+    @Override
+    public List<ProductType> getProductTypes() {
+        Session session = getCurrentSession();
+
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder(session);
+        CriteriaQuery<ProductType> criteriaQuery = getCriteriaQuery(criteriaBuilder, ProductType.class);
+        Root<ProductType> productTypeRoot = getRoot(criteriaQuery, ProductType.class);
+
+        criteriaQuery.select(productTypeRoot);
+
+        return session.createQuery(criteriaQuery).getResultList();
+    }
+
+    @Override
+    public Optional<ProductType> getProductTypeById(int id) {
+        Session session = getCurrentSession();
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder(session);
+        CriteriaQuery<ProductType> criteriaQuery = getCriteriaQuery(criteriaBuilder, ProductType.class);
+        Root<ProductType> productTypeRoot = getRoot(criteriaQuery, ProductType.class);
+
+        criteriaQuery.select(productTypeRoot).where(criteriaBuilder.equal(productTypeRoot.get(PRODUCT_TYPE_ID), id));
+        return session.createQuery(criteriaQuery).getResultList().stream().findFirst();
+    }
+
+    @Override
+    public Optional<ProductType> getProductTypeByName(String name) {
+        Session session = getCurrentSession();
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder(session);
+        CriteriaQuery<ProductType> criteriaQuery = getCriteriaQuery(criteriaBuilder, ProductType.class);
+        Root<ProductType> productTypeRoot = getRoot(criteriaQuery, ProductType.class);
+
+        criteriaQuery.select(productTypeRoot).where(criteriaBuilder.equal(productTypeRoot.get(PRODUCT_TYPE_NAME), name));
+        return session.createQuery(criteriaQuery).getResultList().stream().findFirst();
+    }
+
+    @Override
+    public boolean update(ProductType productType) {
+        Session session = getCurrentSession();
+        session.saveOrUpdate(productType);
+        return true;
+    }
+
+    @Override
+    public boolean remove(int id) {
+        Session session = getCurrentSession();
+        return session.createQuery(DELETE_PRODUCT_TYPE_QUERY)
+                .setParameter(PRODUCT_TYPE_ID, id)
+                .executeUpdate() > 0;
+    }
+}

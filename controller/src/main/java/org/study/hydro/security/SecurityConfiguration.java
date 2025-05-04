bb3644c7437edd1c17b9914078790f13fd0b1752
Alexandr <58393300+Ale5x@users.noreply.github.com>
2025-05-04 10:59:27 +0300

    impoved http configuration. Fix a problem when we got a login page instead of a code 403 and 401

--- controller/src/main/java/org/study/hydro/security/SecurityConfiguration.java
+++ controller/src/main/java/org/study/hydro/security/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package org.study.hydro.security;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,6 +29,10 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final AuthUserDetailsServiceImpl authUserDetailsService;
 
+    private static final String ERROR_FORBIDDEN_MESSAGE = "{\"error\": \"Forbidden\"}";
+    private static final String ERROR_UNAUTHORIZED_MESSAGE = "{\"error\": \"Unauthorized\"}";
+
+
     @Autowired
     public SecurityConfiguration(
             JwtAuthenticationFilter jwtAuthFilter,
@@ -47,15 +53,23 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
+                .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers(PathPages.AUTH_ALL_RESOURCES,  PathPages.GUEST_ALL_RESOURCES)
-                    .permitAll()
+                .requestMatchers(PathPages.AUTH_ALL_RESOURCES, PathPages.GUEST_ALL_RESOURCES)
+                .permitAll()
                 .requestMatchers(PathPages.USER_ALL_RESOURCES)
-                    .hasAnyAuthority(ERole.USER.name(), ERole.ADMIN.name(), ERole.MANAGER.name(), ERole.CEO.name())
+                .hasAnyAuthority(
+                        ERole.USER.name(),
+                        ERole.ADMIN.name(),
+                        ERole.MANAGER.name(),
+                        ERole.CEO.name()
+                )
                 .requestMatchers(PathPages.API_ALL_RESOURCES)
-                    .hasAnyAuthority(ERole.ADMIN.name(), ERole.MANAGER.name(), ERole.CEO.name())
+                .hasAnyAuthority(
+                        ERole.ADMIN.name(),
+                        ERole.MANAGER.name(),
+                        ERole.CEO.name()
+                )
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
@@ -64,8 +78,17 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .oauth2Login(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
-
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                    response.getWriter().write(ERROR_UNAUTHORIZED_MESSAGE);
+                })
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                    response.getWriter().write(ERROR_FORBIDDEN_MESSAGE);
+                });
         return http.build();
     }
 }
